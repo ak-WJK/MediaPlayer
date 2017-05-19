0d0ffa2d5182fb60927f4d8f96ffd299647bd43b
ak-WJK <WJK2288@163.com>
2017-05-19 19:39:00 +0800

    修复视频播放居中问题

--- app/src/main/java/com/atguigu/mediaplayer/fragment/LocalVideoFragment.java
+++ app/src/main/java/com/atguigu/mediaplayer/fragment/LocalVideoFragment.java
@@ -66,6 +66,7 @@ public class LocalVideoFragment extends BaseFragment {
 //            startActivity(intent);
 
 
+
             Intent intent = new Intent(context,SystemVideoPlayerActivity.class);
             intent.setDataAndType(Uri.parse(item.getAddress()), "video/*");
             startActivity(intent);
